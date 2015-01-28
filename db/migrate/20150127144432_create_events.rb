1e5e44ea3a2dfcff3c5ee860fcaffe5238ba7178
Loïc Vigneron <loic.vigneron@gmail.com>
2015-01-28 11:03:23 +0100

    Add migration fields for events

--- db/migrate/20150127144432_create_events.rb
+++ db/migrate/20150127144432_create_events.rb
@@ -1,6 +1,15 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
+      t.string :application_uid, index: true
+      t.string :type, index: true
+      t.string :model_name
+      t.string :controller_name
+      t.string :action_name
+      t.string :action_uid
+      t.datetime :date_time
+      t.text :data
+      t.text :session_data
 
       t.timestamps null: false
     end
