c88374b817d6cb5cc116f99d8130bf0348b55eb5
Loïc Vigneron <loic.vigneron@gmail.com>
2015-02-08 19:14:12 +0100

    Setting RAILS_ENV for whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 every 5.minutes do
-  command "spin42-reporting_rake data_workers:google_analytics"
-  command "spin42-reporting_rake data_workers:new_relic_application"
-  command "spin42-reporting_rake data_workers:new_relic_server"
-  command "spin42-reporting_rake data_workers:twitter"
+  command "RAILS_ENV=#{ENV['RAILS_ENV']} && spin42-reporting_rake data_workers:google_analytics"
+  command "RAILS_ENV=#{ENV['RAILS_ENV']} && spin42-reporting_rake data_workers:new_relic_application"
+  command "RAILS_ENV=#{ENV['RAILS_ENV']} && spin42-reporting_rake data_workers:new_relic_server"
+  command "RAILS_ENV=#{ENV['RAILS_ENV']} && spin42-reporting_rake data_workers:twitter"
 end
