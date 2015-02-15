1dbe3123ed8604a53b29a9062e222b73213c0ea8
Loïc Vigneron <loic.vigneron@gmail.com>
2015-02-15 17:00:50 +0100

    Fix index page

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -3,9 +3,5 @@ class PrivateController < ApplicationController
 
   def index
     @events = Event.order(created_at: :desc).limit(20)
-    @new_relic_server_report_count      = NewRelicServerReport.count
-    @new_relic_application_report_count = NewRelicApplicationReport.count
-    @twitter_report_count               = TwitterReport.count
-    @google_analytics_daily_report_count= GoogleAnalyticsDailyReport.count
   end
 end
