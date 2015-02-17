bcee7377e076190039d3472d509e51b1ab7a427c
Loïc Vigneron <loic.vigneron@gmail.com>
2015-02-17 17:39:22 +0100

    Add authentication to basic api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,16 @@
 class ApiController < ApplicationController
   protect_from_forgery with: :null_session
   layout false
+
+  before_action :authenticate
+
+  private
+  def authenticate
+    api_key     = Base64.decode64 params["api_key"]
+    api_secret  = Base64.decode64 params["api_secret"]
+    application = Application.find_by(api_key: api_key, api_secret: api_secret)
+    unless application
+      render json: { error: "Invalid credentials" }, status: :unauthorized
+    end
+  end
 end
